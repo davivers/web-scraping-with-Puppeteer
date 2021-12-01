f312bcbb9d902c82839c98edede95390ad724b98
David Lima <37056819+davivers@users.noreply.github.com>
2021-11-30 23:36:53 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# Puppeteer Web scraping 
+ Web scraper made with puppeteer, use it by your own risk. 
---
+++ index.js
@@ -0,0 +1,21 @@
+const puppeteer = require ('puppeteer');
+
+(async () => {
+    const browser = await puppeteer.launch({headless: false}); /// can set it to true, so it won't open the browser 
+    const page = await browser.newPage()
+    await page.goto("https://websiteportfolio-m0nzzvsm0-davivers.vercel.app/") //choose website
+
+
+    const grabParagraph = await page.evaluate(() => {
+        const pgTag = document.querySelector(".css-1ivlz9a")    //inside parenthesis the css or any class element you need to scrape
+        return pgTag.innerHTML;                              // can change innerHTML to other functions
+    });
+
+
+
+
+    console.log(grabParagraph);
+    await browser.close();
+})();
+
+
