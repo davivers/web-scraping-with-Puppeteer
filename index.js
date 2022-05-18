541a35982edc12f30ad0882d4930e50876b8d175
David Lima <37056819+davivers@users.noreply.github.com>
2022-05-18 07:32:32 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const puppeteer = require ('puppeteer');
 
 (async () => {
-    const browser = await puppeteer.launch({headless: false}); /// can set it to true, so it won't open the browser 
+    const browser = await puppeteer.launch({headless: true}); /// can set it to true, so it won't open the browser 
     const page = await browser.newPage()
-    await page.goto("https://websiteportfolio-m0nzzvsm0-davivers.vercel.app/") //choose website
+    await page.goto(" ") //choose website
 
 
     const grabParagraph = await page.evaluate(() => {
-        const pgTag = document.querySelector(".css-1ivlz9a")    //inside parenthesis the css or any class element you want to scrape (make sure to add . to the beggining)
+        const pgTag = document.querySelector(" ")    //inside parenthesis the css or any class element you want to scrape (make sure to add . to the beggining)
         return pgTag.innerHTML;                              // can change innerHTML to other functions
     });
 
